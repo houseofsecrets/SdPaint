b5387939026405228abcbea65c7ca7b51f30a9d8
Danamir <danamir@danamir.com>
2023-04-18 00:38:25 +0200

    Added 'e' for eraser, support hr_fix

--- Scripts/SdPaint.py
+++ Scripts/SdPaint.py
@@ -5,6 +5,7 @@ import numpy as np
 import base64
 import io
 import json
+import math
 from PIL import Image, ImageOps
 
 # Initialize Pygame
@@ -15,23 +16,34 @@ url = "http://127.0.0.1:7860"
 prompt = "A painting by Monet"
 seed = 3456456767
 
+# read settings from payload
+with open("payload.json", "r") as f:
+    settings = json.load(f)
+    width = settings.get('width', 512)
+    height = settings.get('height', 512)
+    if settings['enable_hr'] == 'true':
+        width = math.floor(width * settings['hr_scale'])
+        height = math.floor(height * settings['hr_scale'])
+
 # Set up the display
-screen = pygame.display.set_mode((1024, 512))
+screen = pygame.display.set_mode((width*2, height))
 pygame.display.set_caption("Sd Paint")
 # Setup text
 font = pygame.font.SysFont(None, 24)
 text_input = ""
 # Set up the drawing surface
-canvas = pygame.Surface((1024, 512))
-pygame.draw.rect(canvas, (255, 255, 255), (0, 0, 1024, 512))
+canvas = pygame.Surface((width*2, height))
+pygame.draw.rect(canvas, (255, 255, 255), (0, 0, width*2, height))
 
 # Set up the brush
-brush_size = {1: 2, 2: 10}
+brush_size = {1: 2, 2: 10, 'e': 10}
 brush_colors = {
     1: (0, 0, 0),  # Left mouse button color
     2: (255, 255, 255),  # Middle mouse button color
+    'e': (255, 255, 255),  # Eraser color
 }
-brush_pos = {1: None, 2: None}
+brush_pos = {1: None, 2: None, 'e': None}
+eraser_down = False
 
 # Set up flag to check if server is busy or not
 server_busy = False
@@ -52,18 +64,32 @@ while running:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_BACKSPACE:
-                pygame.draw.rect(canvas, (255, 255, 255), (512, 0, 512, 512))
+                pygame.draw.rect(canvas, (255, 255, 255), (width, 0, width, height))
+            elif event.key == pygame.K_e:
+                eraser_down = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_e:
+                eraser_down = False
+                brush_pos['e'] = None
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button in brush_colors:
-                brush_pos[event.button] = event.pos
+            brush_key = event.button
+            if eraser_down:
+                brush_key = 'e'
+            
+            if brush_key in brush_colors:
+                brush_pos[brush_key] = event.pos
         elif event.type == pygame.MOUSEBUTTONUP:
-            if event.button in brush_colors:
-                brush_pos[event.button] = None
-                brush_color = brush_colors[event.button]
+            if event.button in brush_colors or eraser_down:
+                brush_key = event.button
+                if eraser_down:
+                    brush_key = 'e'
+
+                brush_pos[brush_key] = None
+                brush_color = brush_colors[brush_key]
                 # Check if server is busy before sending request
                 if not server_busy:
                     server_busy = True
-                    img = canvas.subsurface(pygame.Rect(512, 0, 512, 512)).copy()
+                    img = canvas.subsurface(pygame.Rect(width, 0, width, height)).copy()
 
                     # Convert the Pygame surface to a PIL image
                     pil_img = Image.frombytes('RGB', img.get_size(), pygame.image.tostring(img, 'RGB'))
@@ -84,6 +110,7 @@ while running:
                     with open("payload.json", "r") as f:
                         payload = json.load(f)
                     payload['controlnet_units'][0]['input_image'] = data
+                    payload['hr_second_pass_steps'] = math.floor(payload['steps'] * payload['denoising_strength'])
                     
                     def send_request():
                         global server_busy
