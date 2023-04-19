23d7828593f9ac090d17853bb33de562a586cebe
Danamir <danamir@danamir.com>
2023-04-19 22:11:14 +0200

    Minor PEP 8 improvements

--- Scripts/SdPaint.py
+++ Scripts/SdPaint.py
@@ -123,15 +123,21 @@ def update_image(image_data):
     global need_redraw
     need_redraw = True
 
+
 def new_random_seed_for_payload():
     global seed
     seed = round(random.random() * 2**32)
+
     with open("payload.json", "r") as f:
         payload = json.load(f)
+
     payload['seed'] = seed
+
     with open("payload.json", "w") as f:
         json.dump(payload, f, indent=4)
+
     return seed
+
 
 # Set up the main loop
 running = True
