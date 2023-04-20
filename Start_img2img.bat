53bc9d8168ed465db8f17af3beb61f46641dabb3
青龍聖者@bdsqlsz <865105819@qq.com>
2023-04-20 13:37:06 +0800

    img2img.bat
    drop img file to auto laungh

---
+++ Start_img2img.bat
@@ -0,0 +1,23 @@
+@echo off
+setlocal enabledelayedexpansion
+pushd %~dp0
+
+REM Check if venv folder exists, create it if not
+if not exist venv (
+    python -m venv venv
+)
+
+REM Activate the virtual environment
+call venv/Scripts/activate.bat
+
+set "var=%~1"
+
+REM Install required packages
+pip install -r requirements.txt
+
+REM Run the script
+
+python Scripts/SdPaint.py --img2img !var!
+
+REM Deactivate the virtual environment
+deactivate
