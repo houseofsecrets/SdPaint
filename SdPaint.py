8840f048a5549808895878f8f53ec9d995579035
Danamir <danamir@danamir.com>
2023-05-09 21:02:37 +0200

    Refactored the pygame script into a class, added a main script for command line call, corrected various methods broken by state use

---
+++ SdPaint.py
@@ -0,0 +1,17 @@
+import argparse
+
+from scripts.views.PygameView.SdPygame import SdPygame
+
+# Read command-line arguments
+if __name__ == '__main__':
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument("--img2img", help="img2img source file")
+
+    args = argParser.parse_args()
+
+    img2img = args.img2img
+    if img2img == '':
+        img2img = '#'  # force load file dialog if launched with --img2img without value
+
+    sd = SdPygame(img2img)
+    sd.main()
